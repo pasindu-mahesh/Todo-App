636a775f8e0d51e136853d9ab98c6d9c6bea51ea
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-06-28 14:13:20 +0530

    feat: add task list and fix errors html and js files

--- app.js
+++ app.js
@@ -6,9 +6,36 @@ const addTask = () => {
 
     if (text){
         tasks.push({ text: text, completed: false});
-    }
-    console.log(tasks);
+        taskInput.value = "";
+        updateTaskList();
+    } 
 };
+
+const updateTaskList = () => {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) =>{
+        const listItem = document.createElement("li");
+
+        listItem.innerHTML = `
+        <div class="taskItem">
+            <div class="task ${task.completed ? "completed" : ""}">
+                <input type="checkbox" class="checkbox" ${
+                    task.completed ? "checked" : ""
+                }/>
+                <p>${task.text}</p>
+            </div>
+            <div class="icons">
+                <img src="./img/edit.png" onClick="editTask(${index})"/>
+                <img src="./img/bin.png" onClick="deleteTask(${index})"/>
+            </div>
+        </div>
+        `;
+    listItem.addEventListener("change", ()=> toggleTestComplete(index));
+        taskList.append(listItem);
+    });
+}
 
 document.getElementById("newTask").addEventListener("click", function(e){
     e.preventDefault();

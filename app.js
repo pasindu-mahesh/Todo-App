97038cd83cee1cbe4b9d1de40105ca0ff35cce13
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-06-29 11:16:41 +0530

    feat: update stetus bar animation

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ const addTask = () => {
         tasks.push({ text: text, completed: false});
         taskInput.value = "";
         updateTaskList();
+        updateStats();
     } 
 };
 
 const toggleTestComplete = (index) => {
     tasks[index].completed = !tasks[index].completed;
     updateTaskList();
+    updateStats();
 };
 
 const deleteTask = (index) => {
     tasks.splice(index, 1);
     updateTaskList();
+    updateStats();
 };
 
 const editTask = (index) => {
@@ -26,6 +29,17 @@ const editTask = (index) => {
     taskInput.value = tasks[index].text;
     tasks.splice(index, 1);
     updateTaskList();
+    updateStats();
+    
+};
+
+const updateStats = () => {
+    const completeTasks = tasks.filter(task=> task.completed).length;
+    const totalTasks = tasks.length;
+    const progress = (completeTasks/totalTasks)*100
+  const progressBar = document.getElementById("progress");
+  
+  progressBar.style.width = `${progress}%`;
 };
 
 const updateTaskList = () => {
